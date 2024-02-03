8eb77ea6f90831fcaf46a47b45144afb04b02f07
Cesar <Cesar@DESKTOP-JBDJSK8>
2024-02-03 13:07:43 +0800

    I think each jsp page should have an individual side bar

---
+++ src/java/com/accountlist/DisableUser.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.accountlist;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Cesar
+ */
+public class DisableUser extends HttpServlet {
+    
+    Connection con;
+    byte[] key;
+    String cypher;
+    
+    public void init(ServletConfig config) throws ServletException {
+            super.init(config);
+            ServletContext context = getServletContext();
+            
+            String pkey = context.getInitParameter("publicKey");
+            this.key = pkey.getBytes();
+            this.cypher = context.getInitParameter("cypher");
+            
+            try {	
+                    Class.forName(context.getInitParameter("driver"));
+                    
+                    String username = context.getInitParameter("username");
+                    String password = context.getInitParameter("password");
+                    String url = config.getInitParameter("url");
+                    
+                    con = DriverManager.getConnection(url, username, password);
+                    
+                    
+                    
+            } catch (SQLException sqle){
+                    System.out.println("SQLException error occured - " 
+                            + sqle.getMessage());
+            } catch (ClassNotFoundException nfe){
+                    System.out.println("ClassNotFoundException error occured - " 
+                    + nfe.getMessage());
+            }
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int userId = Integer.parseInt(request.getParameter("id"));
+        String query = "UPDATE LOGIN SET DISABLED = TRUE WHERE ID = ?";
+        
+        try
+        {
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, userId);
+            ps.executeUpdate();
+        } 
+        catch (SQLException ex) 
+        {
+            Logger.getLogger(DisableUser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        request.getRequestDispatcher("AccountList").forward(request,response);
+    }
+
+}
